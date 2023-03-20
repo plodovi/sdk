c83606b3a19e3c9bc027fdc83c30704bbba41feb
flauc <filip.lauc93@gmail.com>
2023-03-20 12:16:46 +0100

    feat: initial setup for versioning

---
+++ src/ce-add-to-cart.ts
@@ -0,0 +1,3 @@
+import { AddToCart } from './AddToCart.js';
+
+window.customElements.define('plodovi-add-to-cart', AddToCart);
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { Products } from './Products.js';
 export { Product } from './Product.js';
 export { Cart } from './Cart.js';
 export { CartToggle } from './CartToggle.js';
+export { AddToCart } from './AddToCart.js';
 export { init } from './init.js';
